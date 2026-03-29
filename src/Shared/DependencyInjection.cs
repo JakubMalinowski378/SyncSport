71b5f24debd0d54ab07f4adc877e64df839fd413
Jakub Malinowski <jakubmalinowski378@gmail.com>
2026-03-29 01:00:56 +0100

    Add Swagger support and update package references for Dependency Injection

--- src/Shared/DependencyInjection.cs
+++ src/Shared/DependencyInjection.cs
@@ -9,12 +9,15 @@ public static class DependencyInjection
     public static IServiceCollection AddSharedFramework(this IServiceCollection services)
     {
         services.AddCarter();
+        services.AddSwaggerGen();
 
         return services;
     }
 
     public static WebApplication UseSharedFramework(this WebApplication app)
     {
+        app.UseSwagger();
+        app.UseSwaggerUI();
         app.MapCarter();
 
         return app;
