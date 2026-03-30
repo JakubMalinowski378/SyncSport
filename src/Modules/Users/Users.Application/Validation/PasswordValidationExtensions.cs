4750608625b8d8f8eb4ddc2e0ae995591239324a
Jakub Malinowski <jakubmalinowski378@gmail.com>
2026-03-30 23:57:35 +0200

    Add password validation extension method

---
+++ src/Modules/Users/Users.Application/Validation/PasswordValidationExtensions.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace Users.Application.Validation;
+
+public static class PasswordValidationExtensions
+{
+    public static IRuleBuilderOptions<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+    }
+}
