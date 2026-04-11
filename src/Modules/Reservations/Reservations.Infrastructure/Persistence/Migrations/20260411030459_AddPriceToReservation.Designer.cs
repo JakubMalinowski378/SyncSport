897673c1c1ad9f55425cd8202707edfb6e33f136
Jakub Malinowski <jakubmalinowski378@gmail.com>
2026-04-11 05:07:30 +0200

    Add Price column to Reservations table and update model snapshot

--- src/Modules/Reservations/Reservations.Infrastructure/Persistence/Migrations/20260411030459_AddPriceToReservation.Designer.cs
+++ src/Modules/Reservations/Reservations.Infrastructure/Persistence/Migrations/20260411030459_AddPriceToReservation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Reservations.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using Reservations.Infrastructure.Persistence;
 namespace Reservations.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ReservationsDbContext))]
-    partial class ReservationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260411030459_AddPriceToReservation")]
+    partial class AddPriceToReservation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
